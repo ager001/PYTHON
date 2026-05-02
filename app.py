11eed5fb1646346cddea66332d98ac62b6091f95
ager001 <152964009+ager001@users.noreply.github.com>
2026-05-02 17:41:30 +0300

    add quiz_game.py with initial quiz functionality

--- app.py
+++
@@ -1,4 +0,0 @@
-def hello_func(greeting, name = "You"):
-    return "{}, {} ".format(greeting, name)
-
-print(hello_func(greeting="Hello", name= "Midenga"))
---
+++ quiz_game.py
@@ -0,0 +1,21 @@
+print ("Welcome to my quiz game!")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes" :
+    quit()
+else:    
+    print("Okay! Let's play :)")
+    
+answer = input ("What does CPU stand for? ")
+if answer == "Central Processing Unit" : 
+    print("Correct!")
+else:
+    print("Incorrect! The correct answer is Central Processing Unit.")
+    
+
+answer = input ("Name the capital city of Kenya? ")
+if answer == "Nairobi" :
+    print("Correct!")
+else:
+    print("Incorrect! The correct answer is Nairobi.")
