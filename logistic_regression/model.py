d5647f1dfefb0d7c4a45d525076005ca14583d3f
ager001 <152964009+ager001@users.noreply.github.com>
2026-05-18 12:58:06 +0300

    add: update model to use logistic regression for pass/fail prediction based on study hours

--- logistic_regression/model.py
+++ logistic_regression/model.py
@@ -1,35 +1,33 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 
 # Study hours
-X = np.array([1, 2, 3, 4, 5, 6]).reshape(-1, 1)
+X = np.array([1, 2, 3, 4, 5, 6, 7, 8]).reshape(-1, 1)
 
-# Exam scores
-y = np.array([30, 40, 50, 60, 70, 80])
+# Results
+# 0 = Fail
+# 1 = Pass
+y = np.array([0, 0, 0, 0, 1, 1, 1, 1])
 
 # Create model
-model = LinearRegression()
+model = LogisticRegression()
 
 # Train model
 model.fit(X, y)
 
-# Predict score for 7 study hours
-prediction = model.predict([[7]])
+# Predict pass/fail for student studying 5 hours
+prediction = model.predict([[5]])
 
-print("Predicted Score:", prediction[0])
+# Predict probability
+probability = model.predict_proba([[5]])
 
-# Predict values for drawing regression line
-y_pred = model.predict(X)
+print("Prediction:", prediction[0])
 
-# Plot original data
-plt.scatter(X, y)
+if prediction[0] == 1:
+    print("Student will PASS")
+else:
+    print("Student will FAIL")
 
-# Plot regression line
-plt.plot(X, y_pred)
-
-plt.xlabel("Study Hours")
-plt.ylabel("Exam Score")
-plt.title("Linear Regression Example")
-
-plt.show()
+print("\nProbabilities:")
+print("Fail Probability:", probability[0][0])
+print("Pass Probability:", probability[0][1])
