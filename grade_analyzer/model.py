78a564b03bcb147246dd8c5e94e2bb53d1282b41
ager001 <152964009+ager001@users.noreply.github.com>
2026-06-11 10:32:40 +0300

    add: enhance read_grades function with CSV reading and error handling improvements

--- grade_analyzer/model.py
+++ grade_analyzer/model.py
@@ -2,29 +2,30 @@
 # STUDENT GRADE ANALYZER
 # ==========================================
 
+import csv
+
+
 def read_grades(filename="grades.csv"):
     """
     Reads grades from a CSV file and returns a list of grades.
     """
-
+# Empty list to store valid grades
     grades = []
-
+# This is an exception handling block to catch file not found errors and invalid data formats
     try:
-        with open(filename, "r") as file:
+        with open(filename, "r", encoding="utf-8") as file:
+            
+            reader = csv.reader(file)
 
             # Skip header row
-            next(file)
+            next(reader, None)
 
-            for line in file:
-
-                line = line.strip()
-
-                if not line:
+            for row in reader:
+                if not row:
                     continue
 
                 try:
-                    name, grade = line.split(",")
-
+                    name, grade = row
                     grade = int(grade)
 
                     if grade < 0 or grade > 100:
@@ -32,11 +33,68 @@ def read_grades(filename="grades.csv"):
                         continue
 
                     grades.append(grade)
-
                 except ValueError:
-                    print(f"Invalid row skipped: {line}")
+                    print(f"Invalid row skipped: {row}")
 
     except FileNotFoundError:
         print(f"Error: '{filename}' not found.")
 
     return grades
+
+def count_students(grades):
+    """
+    Returns the total number of students.
+    """
+    return len(grades)
+
+
+def calculate_mean(grades):
+    """
+    Returns the average grade.
+    """
+    if not grades:
+        return 0
+
+    return sum(grades) / len(grades)
+
+
+def calculate_median(grades):
+    """
+    Returns the median grade.
+    """
+
+    if not grades:
+        return 0
+
+    sorted_grades = sorted(grades)
+
+    n = len(sorted_grades)
+
+    if n % 2 == 0:
+
+        middle1 = sorted_grades[n // 2 - 1]
+        middle2 = sorted_grades[n // 2]
+
+        return (middle1 + middle2) / 2
+
+    return sorted_grades[n // 2]
+
+
+def highest_grade(grades):
+    """
+    Returns the highest grade.
+    """
+    if not grades:
+        return 0
+
+    return max(grades)
+
+
+def lowest_grade(grades):
+    """
+    Returns the lowest grade.
+    """
+    if not grades:
+        return 0
+
+    return min(grades)
